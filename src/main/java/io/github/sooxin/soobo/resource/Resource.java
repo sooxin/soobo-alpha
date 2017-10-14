8bf8404eb36b0798b438d8a7e8e50147251d1ff4
苏辛 <ojeniuso@hotmail.com>
2017-10-14 18:24:56 +0800

    update Resource class

--- src/main/java/io/github/sooxin/soobo/resource/Resource.java
+++ src/main/java/io/github/sooxin/soobo/resource/Resource.java
@@ -1,37 +1,35 @@
 package io.github.sooxin.soobo.resource;
 
-import java.io.File;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import io.github.sooxin.soobo.cli.model.CLIConfig;
-import io.github.sooxin.soobo.core.io.FileInput;
-import io.github.sooxin.soobo.core.io.FileOutput;
-
+import io.github.sooxin.soobo.core.model.WebSiteConfig;
+/**
+ * 配置资源类（暂时未使用）
+ * @author Sooxin
+ *
+ */
 public class Resource {
-	private static CLIConfig cliConfig;
-	static {
-		File file = new File("soobo-cli.json");
-		if (!file.exists()) {
-			System.out.println("> 正在创建 soobo-cli.json 配置文件...");
-			Resource.loadSooboCLIResource();
-			System.out.println("> soobo-cli.json 配置文件创建成功！");
+	private static WebSiteConfig webSiteConfig; // 网站配置
+	private static CLIConfig cliConfig;  // CLI 工具配置
+	
+	/**
+	 * 获取网站配置单例
+	 * @return	网站配置单例
+	 */
+	public WebSiteConfig getWebSiteConfigInstance() {
+		if(webSiteConfig==null) {
+			webSiteConfig=WebSiteConfig.getParsedWebSiteConfig();
 		}
-		Resource.cliConfig=new Gson().fromJson(new FileInput().getFileContent("soobo-cli.json"), CLIConfig.class);
+		return webSiteConfig;
 	}
-
-	public static void loadWebSiteResources() {
-
-	}
-
-	public static void loadProjectResource() {
-		
-	}
-
-	public static void loadSooboCLIResource() {
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		new FileOutput().saveFile("soobo-cli.json", gson.toJson(new CLIConfig()));
+	/**
+	 * 获取 CLI 配置单例
+	 * @return	 CLI 配置单例
+	 */
+	public CLIConfig getCLIConfigInstance() {
+		if(cliConfig==null) {
+			CLIConfig.getParsedCLIConfig();
+		}
+		return cliConfig;
 	}
 
 }
